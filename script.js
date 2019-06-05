618334216f81f1b2cdb5774c1352f4131af82bc7
mrasinski <ariel_6@wp.pl>
2019-06-05 11:26:41 +0200

    update CRUD

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://still-harbor-11119.herokuapp.com/v1/task/';
+  const apiRoot = 'https://still-harbor-11119.herokuapp.com/v1/tasks/';
   const trelloApiRoot = 'https://still-harbor-11119.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
